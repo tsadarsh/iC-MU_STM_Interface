a7848513e0dac65a8c5ebd3b6dcd8521785f1a40
Adarsh TS <tsadarsh0707@gmail.com>
2023-07-05 11:54:52 -0400

    Added input for port selection and status light

--- gui/gui.py
+++ gui/gui.py
@@ -2,10 +2,52 @@ import dearpygui.dearpygui as dpg
 import serial
 import re
 from time import sleep
+
 CMD_T0_SEND = 'A6'
-ser = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
+ser = None
 dpg.create_context()
 dpg.create_viewport(title='iC-MU Debug Tool')
+
+texture_data = []
+for i in range(0, 10 * 10):
+    texture_data.append(255 / 255)
+    texture_data.append(255)
+    texture_data.append(255 / 255)
+    texture_data.append(255 / 255)
+with dpg.texture_registry(show=False):
+	dpg.add_dynamic_texture(width=10, height=10, default_value=texture_data, tag="UART_PORT_STATUS")
+
+def _update_dynamic_textures(status):
+	if status == "success":
+		new_texture_data = []
+		for i in range(0, 10 * 10):
+			new_texture_data.append(0)
+			new_texture_data.append(255)
+			new_texture_data.append(0)
+			new_texture_data.append(255)
+
+		dpg.set_value("UART_PORT_STATUS", new_texture_data)
+	
+	if status == "fail":
+		new_texture_data = []
+		for i in range(0, 10 * 10):
+			new_texture_data.append(255)
+			new_texture_data.append(0)
+			new_texture_data.append(0)
+			new_texture_data.append(255)
+
+		dpg.set_value("UART_PORT_STATUS", new_texture_data)
+
+def restart_serial(sender, app_data):
+	print("restarting...")
+	print(app_data)
+	global ser
+	try:
+		ser = serial.Serial(port=app_data, baudrate=115200)
+		_update_dynamic_textures("success")
+	except serial.serialutil.SerialException:
+		_update_dynamic_textures("fail")
+	
 
 def activateCallback(sender, app_data):
 	CMD_T0_SEND = 'A6'
@@ -18,6 +60,8 @@ def activateCallback(sender, app_data):
 	dpg.set_value("tick", data.decode())
 
 with dpg.window(label="Interact"):
+	dpg.add_input_text(default_value="/dev/ttyACM0", callback=restart_serial)
+	dpg.add_image("UART_PORT_STATUS")
 	dpg.add_text("Start", tag="tick")
 	dpg.add_button(label="UPDATE", tag="update", callback=activateCallback)
 
